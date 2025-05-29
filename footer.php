40b69e940ec129c98ebc88a3ea0ce9bbcb2f753f
MTAR <rayanemtar2@gmail.com>
2025-05-29 18:06:29 +0100

    v2 test

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- footer.php -->
+    <footer>
+        <div class="footer-content">
+            <div class="footer-logo-text">Immo<span>Tunis</span></div>
+            <div class="social-links">
+                <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-youtube"></i></a>
+            </div>
+            <p class="copyright">&copy; 2023 ImmoTunis. Tous droits réservés.</p>
+        </div>
+    </footer>
